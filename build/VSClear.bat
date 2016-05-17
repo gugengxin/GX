6d55af52253c4738f6b27a49c13f48ecb3de82cb
GuGengxin <gugengxin@gmail.com>
2016-05-17 18:22:39 +0800

    vsclear++

--- build/VSClear.bat
+++ build/VSClear.bat
@@ -12,5 +12,4 @@ cd \
 @REG Delete HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\12.0\FileMRUList /va /f
 @REG Delete HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\12.0\ProjectMRUList /va /f
 @echo off
-cmd
-@echo on
+pause
