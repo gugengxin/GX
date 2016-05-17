f55641cd8c2444245cb9218c1923f71115f2173f
GuGengxin <gugengxin@gmail.com>
2016-05-18 01:44:23 +0800

    sl++

--- tool/cmd/shared_src/GCSL.cpp
+++ tool/cmd/shared_src/GCSL.cpp
@@ -3,20 +3,78 @@
 /*
 gxsl(GX Shading Language) 将被翻译成 glsl或hlsl
 
-a:vec3
+lowp medi high
+float
+vec2 vec3 vec4
+mat2 mat3 mat4
+tex1d tex2d
+input
+buffer
+bridge
+POSITION[0-9]
+TEXCOORD[0-9]
+register(b0,t0)
+
+
+#def
+#if
+#else
+#elif
+#endif
+void
+#vs
+fp
+main
+
+gx_Position
+gx_FragColor
+
+;
+()
+[]
+{}
+,
+=
++
+-
+*
+/
+
+x.x
 
 
 
-void vs_main()
-{
+
+
+
+vs {
+input {
+    vec4 pos:POSITION;
+    vec2 tex:TEXCOORD0;
+    vec2 texMask:TEXCOORD0;
+}
+buffer {
+    mat4 mvpMatrix;
+}
+bridge {
 
 }
 
-void fp_main()
+void main()
 {
 
 }
+}
+fp {
+buffer {
 
+}
+
+void main()
+{
+
+}
+}
 //*/
 
 
