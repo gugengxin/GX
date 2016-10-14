d96623cc297c82972d93d4dffae26acb8ca6dec4
Gengxin Gu <gugengxin@gmail.com>
2016-10-14 13:38:13 +0800

    frame buffer start

---
+++ src/GFrameBuffer.cpp
@@ -0,0 +1,26 @@
+﻿//
+//  GFrameBuffer.cpp
+//  GX
+//
+//  Created by GuGengxin.
+// Copyright by gxengine.com
+//
+
+#include "GFrameBuffer.h"
+//Down include other h file
+
+//Up include other h file
+#include "GXGObject.h"
+
+GX_GOBJECT_IMPLEMENT(GFrameBuffer, GObject);
+
+GFrameBuffer::GFrameBuffer()
+{
+	
+}
+
+GFrameBuffer::~GFrameBuffer()
+{
+}
+
+
---
+++ src/GFrameBuffer.h
@@ -0,0 +1,32 @@
+﻿//
+//  GFrameBuffer.h
+//  GX
+//
+//  Created by GuGengxin.
+// Copyright by gxengine.com
+//
+
+#ifndef GFrameBuffer_h
+#define GFrameBuffer_h
+
+#include "GXPrefix.h"
+#include "GObject.h"
+
+
+
+#include "GXGObject.h"
+// Down can't include other h file
+
+class GFrameBuffer : public GObject
+{
+	GX_GOBJECT(GFrameBuffer);
+public:
+
+private:
+
+};
+
+// Up can't include other h file
+#include "GXGObjectUD.h"
+
+#endif /* GFrameBuffer_h */
