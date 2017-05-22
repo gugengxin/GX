124647de8906197487002e738f28c9e13bf94347
Gu Gengxin <gugengxin@gmail.com>
2017-05-22 10:12:42 +0800

    ++

--- core/src/GTypist.cpp
+++ core/src/GTypist.cpp
@@ -10,7 +10,28 @@
 
 GSizeF GTypist::sizeOfString(GString * str, GFont * font)
 {
-
-
-	return GSizeF();
+    GSizeF res(0.0f, font->getHeight());
+    gint len=str->getLength();
+    if (len>0) {
+        bool use_kerning=font->hasKerning();
+        guint32 previous=0;
+        guint32 glyph_index;
+        
+        for (gint i=0; i<len; i++) {
+            GFont::Glyph* glyph=GX_CAST_R(GFont::Glyph*, font->getGlyph(str->at(i)));
+            if (!glyph) {
+                continue;
+            }
+            glyph_index=glyph->getIndex();
+            
+            if (use_kerning && previous && glyph_index ) {
+                res.width += font->getKerningX(previous, glyph_index);
+            }
+            
+            res.width+=glyph->getAdvanceX();
+            previous=glyph_index;
+        }
+    }
+    
+    return res;
 }
