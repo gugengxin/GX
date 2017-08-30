59bd25df39699d9e065bae95c3777efbd612eb0e
GuGengxin <gugengxin@gmail.com>
2017-08-30 23:47:57 +0800

    freetype android win build done but not h files

--- external/freetype/project/android/build.bat
+++ external/freetype/project/android/build.bat
@@ -10,27 +10,58 @@ pushd %LIBRARY_ROOT%\..\..
 set GX_ROOT=%cd%
 popd
 
-set OUTPUT_DIR=%LIBRARY_ROOT%\lib\ard
+set SRC_ROOT=%LIBRARY_ROOT%\src
 
+call %GX_ROOT%\bin\cmake_.bat
+call %GX_ROOT%\bin\ninja_.bat
+call %GX_ROOT%\bin\ndk.bat
+
+
+set OUTPUT_DIR=%LIBRARY_ROOT%\lib\ard
 if exist %OUTPUT_DIR% (
 	rd /s/q %OUTPUT_DIR%
 )
 mkdir %OUTPUT_DIR%
 
-call %GX_ROOT%\bin\ndk.bat
+set TARGET_NAME=freetype
 
-call ndk-build.cmd NDK_PROJECT_PATH=%PROJECT_ROOT%
- 
-for /r %PROJECT_ROOT%\obj\local %%i in (*.a) do (
-	for %%j in ("%%~dpi.") do (
-		mkdir %OUTPUT_DIR%\%%~nj
-		move %%i %OUTPUT_DIR%\%%~nj
-	)
+if exist "%PROJECT_ROOT%\build" (
+	rd /s/q %PROJECT_ROOT%\build
 )
-::copy %%i %OUTPUT_DIR%\%%~nj\%%~nxi
+mkdir %PROJECT_ROOT%\build
+pushd %PROJECT_ROOT%\build
 
-rd /s/q %PROJECT_ROOT%\obj
+for %%i in ( "armeabi","armeabi-v7a","arm64-v8a","x86","x86_64","mips","mips64" ) do (
+	
+	setlocal EnableDelayedExpansion
+	
+	set ARCH_ABI=%%i
+	
+	if !ARCH_ABI!=="arm64-v8a" (
+		set API_LEVEL=21
+	) else if !ARCH_ABI!=="x86_64" (
+		set API_LEVEL=21
+	) else if !ARCH_ABI!=="mips64" (
+		set API_LEVEL=21
+	) else (
+		set API_LEVEL=14
+	)
+	
+	mkdir !ARCH_ABI!
+	pushd !ARCH_ABI!
+	cmake -GNinja -D"CMAKE_TOOLCHAIN_FILE=%GX_ROOT%\bin\android.toolchain.cmake" -D"CMAKE_ANDROID_NDK=%ANDROID_NDK_ROOT_FOR_CMAKE%" -D"CMAKE_ANDROID_ARCH_ABI=!ARCH_ABI!" -D"CMAKE_SYSTEM_VERSION=!API_LEVEL!" -D"CMAKE_BUILD_TYPE=Release" -D"FREETYPE_NO_DIST=true" -D"WITH_ZLIB=OFF" -D"WITH_BZip2=OFF" -D"WITH_PNG=OFF" -D"WITH_HarfBuzz=OFF" %SRC_ROOT%
+	ninja -C .
+	mkdir %OUTPUT_DIR%\!ARCH_ABI!
+	move lib%TARGET_NAME%.a %OUTPUT_DIR%\!ARCH_ABI!\lib%TARGET_NAME%.a
+	popd
+	
+	endlocal
+)
 
-copy %PROJECT_ROOT%\libAndroid.mk %OUTPUT_DIR%\Android.mk
+popd
+
+copy /y %PROJECT_ROOT%\libAndroid.mk %OUTPUT_DIR%\Android.mk
+
+rd /s/q %PROJECT_ROOT%\build
 
 goto :EOF
--- external/freetype/project/android/libAndroid.mk
+++ external/freetype/project/android/libAndroid.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := gx_freetype
 LOCAL_MODULE_FILENAME := freetype
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/freetype.a
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libfreetype.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
