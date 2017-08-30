b4704c3cbb2d8be55047e5d56848d5d96f3d5e83
GuGengxin <gugengxin@gmail.com>
2017-08-30 09:15:57 +0800

    freetype 2.8 start: win build done 但还差头文件生成

--- external/freetype/project/vs/build.bat
+++ external/freetype/project/vs/build.bat
@@ -10,11 +10,28 @@ pushd %LIBRARY_ROOT%\..\..
 set GX_ROOT=%cd%
 popd
 
+set GX_TE=%GX_ROOT%\bin\gx_te.exe
+set SRC_ROOT=%LIBRARY_ROOT%\src
+
+call %GX_ROOT%\bin\cmake_.bat
 call %GX_ROOT%\bin\vc.bat
 
-msbuild %PROJECT_ROOT%\freetype.sln /t:Rebuild /p:Configuration=Debug,Platform=Win32
-msbuild %PROJECT_ROOT%\freetype.sln /t:Rebuild /p:Configuration=Release,Platform=Win32
-msbuild %PROJECT_ROOT%\freetype.sln /t:Rebuild /p:Configuration=Debug,Platform=x64
-msbuild %PROJECT_ROOT%\freetype.sln /t:Rebuild /p:Configuration=Release,Platform=x64
+mkdir %PROJECT_ROOT%\build
+pushd %PROJECT_ROOT%\build
+cmake -G"Visual Studio 14 2015" -D"FREETYPE_NO_DIST=true" -D"WITH_ZLIB=OFF" -D"WITH_BZip2=OFF" -D"WITH_PNG=OFF" -D"WITH_HarfBuzz=OFF" %SRC_ROOT%
+msbuild freetype.vcxproj /t:Rebuild /p:Configuration=Release,OutDir=..\..\..\lib\win\vc\x86\Release\
+%GX_TE% -i freetype.vcxproj -o freetype.vcxproj -rs "<AssemblerListingLocation>Debug/</AssemblerListingLocation>" -rd "<AssemblerListingLocation>Debug/</AssemblerListingLocation><ProgramDataBaseFileName>$(OutDir)$(TargetName)_vc$(PlatformToolsetVersion).pdb</ProgramDataBaseFileName>"
+msbuild freetype.vcxproj /t:Rebuild /p:Configuration=Debug,OutDir=..\..\..\lib\win\vc\x86\Debug\
+popd
+rd /s/q %PROJECT_ROOT%\build
+
+mkdir %PROJECT_ROOT%\build
+pushd %PROJECT_ROOT%\build
+cmake -G"Visual Studio 14 2015 Win64" -D"FREETYPE_NO_DIST=true" -D"WITH_ZLIB=OFF" -D"WITH_BZip2=OFF" -D"WITH_PNG=OFF" -D"WITH_HarfBuzz=OFF" %SRC_ROOT%
+msbuild freetype.vcxproj /t:Rebuild /p:Configuration=Release,OutDir=..\..\..\lib\win\vc\x64\Release\
+%GX_TE% -i freetype.vcxproj -o freetype.vcxproj -rs "<AssemblerListingLocation>Debug/</AssemblerListingLocation>" -rd "<AssemblerListingLocation>Debug/</AssemblerListingLocation><ProgramDataBaseFileName>$(OutDir)$(TargetName)_vc$(PlatformToolsetVersion).pdb</ProgramDataBaseFileName>"
+msbuild freetype.vcxproj /t:Rebuild /p:Configuration=Debug,OutDir=..\..\..\lib\win\vc\x64\Debug\
+popd
+rd /s/q %PROJECT_ROOT%\build
 
 goto :EOF
