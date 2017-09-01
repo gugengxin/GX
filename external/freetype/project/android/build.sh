3ca232c37c02248670cc42b59d8f14178ed50077
GuGengxin <gugengxin@didichuxing.com>
2017-09-01 08:43:49 +0800

    freetype build mac done

--- external/freetype/project/android/build.sh
+++ external/freetype/project/android/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 cd $(dirname $0)
 PROJECT_ROOT=$PWD
@@ -6,10 +7,24 @@ cd ../..
 LIBRARY_ROOT=$PWD
 cd ../..
 GX_ROOT=$PWD
+cd $PROJECT_ROOT
 
+SRC_ROOT=$LIBRARY_ROOT/src
+
+source $GX_ROOT/bin/cmake.sh
+source $GX_ROOT/bin/ninja.sh
 source $GX_ROOT/bin/ndk.sh
 
-ndk-build NDK_PROJECT_PATH=$PROJECT_ROOT
+function copyInclude {
+	OUTPUT_H_DIR="$1"
+	CMAKE_CACHE_DIR="$2"
+	if [ -d $OUTPUT_H_DIR ]; then
+		rm -rf $OUTPUT_H_DIR
+	fi
+	cp -r ${SRC_ROOT}/include $OUTPUT_H_DIR
+	cp -f ${CMAKE_CACHE_DIR}/include/freetype/config/ftconfig.h $OUTPUT_H_DIR/freetype/config/ftconfig.h
+	cp -f ${CMAKE_CACHE_DIR}/include/freetype/config/ftoption.h $OUTPUT_H_DIR/freetype/config/ftoption.h
+}
 
 OUTPUT_DIR=$LIBRARY_ROOT/lib/ard
 if [ -d $OUTPUT_DIR ] 
@@ -18,19 +33,39 @@ rm -rf $OUTPUT_DIR
 fi
 mkdir -p $OUTPUT_DIR
 
-function move_lib {
-	TEMP_OUT_DIR=$2/$(basename $(dirname $1))
-	if [ ! -d $TEMP_OUT_DIR ] 
-	then
-		mkdir $TEMP_OUT_DIR
-	fi
-	mv $1 $TEMP_OUT_DIR
-}
-export -f move_lib
-find $PROJECT_ROOT/obj/local -name "*.a" -exec bash -c "move_lib {} $OUTPUT_DIR" \;
+TARGET_NAME=freetype
 
-rm -rf $PROJECT_ROOT/obj
+mkdir build
+cd build
+
+ARCH_ABIs=("armeabi" "armeabi-v7a" "arm64-v8a" "x86" "x86_64" "mips" "mips64")
+for ((i=0;i<${#ARCH_ABIs[@]};i++))
+do
+	ARCH_ABI=${ARCH_ABIs[$i]}
+	if [ ${ARCH_ABI} == "arm64-v8a" ] || [ ${ARCH_ABI} == "x86_64" ] || [ ${ARCH_ABI} == "mips64" ]
+	then
+		API_LEVEL=21
+	else
+		API_LEVEL=14
+	fi
+	mkdir ${ARCH_ABI}
+	cd ${ARCH_ABI}
+	cmake -GNinja -D"CMAKE_TOOLCHAIN_FILE=${GX_ROOT}/bin/android.toolchain.cmake" -D"CMAKE_ANDROID_NDK=${ANDROID_NDK_ROOT}" -D"CMAKE_ANDROID_ARCH_ABI=${ARCH_ABI}" -D"CMAKE_SYSTEM_VERSION=${API_LEVEL}" -D"CMAKE_BUILD_TYPE=Release" -D"FREETYPE_NO_DIST=true" -D"WITH_ZLIB=OFF" -D"WITH_BZip2=OFF" -D"WITH_PNG=OFF" -D"WITH_HarfBuzz=OFF" ${SRC_ROOT}
+	ninja -C .
+	mkdir -p $OUTPUT_DIR/${ARCH_ABI}
+	mv lib${TARGET_NAME}.a $OUTPUT_DIR/${ARCH_ABI}/lib${TARGET_NAME}.a
+	cd ..
+done
+
+cd ..
+
 cp $PROJECT_ROOT/libAndroid.mk $OUTPUT_DIR/Android.mk
+
+copyInclude $LIBRARY_ROOT/include/ard $PROJECT_ROOT/build/armeabi
+
+rm -rf build
+
+
 
 
 
--- external/freetype/project/xcode/build.sh
+++ external/freetype/project/xcode/build.sh
@@ -15,6 +15,17 @@ source $GX_ROOT/bin/ninja.sh
 
 SRC_ROOT=${LIBRARY_ROOT}/src
 
+function copyInclude {
+	OUTPUT_H_DIR="$1"
+	CMAKE_CACHE_DIR="$2"
+	if [ -d $OUTPUT_H_DIR ]; then
+		rm -rf $OUTPUT_H_DIR
+	fi
+	cp -r ${SRC_ROOT}/include $OUTPUT_H_DIR
+	cp -f ${CMAKE_CACHE_DIR}/include/freetype/config/ftconfig.h $OUTPUT_H_DIR/freetype/config/ftconfig.h
+	cp -f ${CMAKE_CACHE_DIR}/include/freetype/config/ftoption.h $OUTPUT_H_DIR/freetype/config/ftoption.h
+}
+
 #iOS
 mkdir -p build/ios/device
 cd build/ios/device
@@ -42,6 +53,8 @@ fi
 mkdir -p $OUTPUT_DIR
 lipo -create build/ios/device/libfreetype.a build/ios/simulator/libfreetype.a build/ios/simulator64/libfreetype.a -output $OUTPUT_DIR/libfreetype.a
 
+copyInclude $LIBRARY_ROOT/include/ios $PROJECT_ROOT/build/ios/device
+
 #TVOS
 mkdir -p build/atv/device
 cd build/atv/device
@@ -63,6 +76,7 @@ fi
 mkdir -p $OUTPUT_DIR
 lipo -create build/atv/device/libfreetype.a build/atv/simulator/libfreetype.a -output $OUTPUT_DIR/libfreetype.a
 
+copyInclude $LIBRARY_ROOT/include/atv $PROJECT_ROOT/build/atv/device
 
 #MacOSX
 mkdir -p build/mac
@@ -79,5 +93,6 @@ fi
 mkdir -p $OUTPUT_DIR
 lipo -create build/mac/libfreetype.a -output $OUTPUT_DIR/libfreetype.a
 
+copyInclude $LIBRARY_ROOT/include/mac $PROJECT_ROOT/build/mac
 
 rm -rf build
